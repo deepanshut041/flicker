481ec17ab4f6497099b181b42ea373082b03cbd8
deepanshut041 <deepanshut041@gmail.com>
2017-09-05 00:32:21 +0530

    Listview is populated with array adapter

--- app/src/main/java/com/example/deepn/flicker/ForecastFragment.java
+++ app/src/main/java/com/example/deepn/flicker/ForecastFragment.java
@@ -6,13 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ForecastFragment extends Fragment {
-
+    public ArrayAdapter<String> mForecastAdapter ;
 
     public ForecastFragment() {
         // Required empty public constructor
@@ -23,7 +27,20 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forecast, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.forecastList);
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < 20; i++){
+            arrayList.add("List item " + i);
+        }
+        mForecastAdapter = new ArrayAdapter<String>(getContext(),
+                R.layout.forecast_textview,
+                R.id.forcasttextview,
+                arrayList);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 
 }
